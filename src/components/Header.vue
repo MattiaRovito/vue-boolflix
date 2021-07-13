7fdff30a813d5b0745e1c093cc6d80f702c43817
MattiaR <rovitomattia91@gmail.com>
2021-07-13 16:27:04 +0200

    modalità provvisoria

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,14 @@
 <template>
     <header>
-        <div class="container">
-            <div class="logo">Boolflix</div>
+        <div class="container flex nav justify-content-between p-3">
+            <h1 class="logo mt-1 fw-bold"><strong class="red">Bool</strong><strong>flix</strong></h1>
 
             <input 
             type="text" 
             placeholder="Cerca i tuoi film e serie tv"
             v-model="search"
-            @keyup="$emit('ricerca', search)">
+            @keyup="$emit('ricerca', search)"
+            class="mt-3 form-control">
             <!-- al posto del keyup si può usare il bottone con il @click -->
         </div>
 
@@ -33,8 +34,38 @@ export default {
     {
         height: 90px;
         background-color: #020202;
+
+        .red
+        {
+            color: red;
+        }
+
+        input
+        {
+            height: 30px;
+            width: 20%;
+            transition: width 1s;
+
+            &:hover
+            {
+                width:25%;
+            }
+        }
+    }
+    .logo
+    {
+        
+        color:white
     }
 
 
 
 </style>
+
+
+ transition: width 2s;
+}
+
+.searchicon:hover~.search{
+  
+}
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,9 +2,19 @@
   <main>
       <div class="container">
           <div class="search">
-              <h2>Hai cercato: {{campoRicerca}} </h2>
+              <h2>Hai cercato: {{capitalize(campoRicerca)}} </h2>
               <div class="cards">
-                  <Card v-for="film in films" :key="film.id" :info="film"/>
+                  <!-- <Card  v-for="film in films" :key="film.id" :info="film"/> -->
+                    <div class="cards">
+                        <div v-for="(film, index) in films" :key="index">
+                            <ul>
+                                <li>Titolo: {{film.title}}</li>
+                                <li>Titolo originale: {{film.original_title}}</li>
+                                <li>Lingua: {{film.original_language}}</li>
+                                <li>Voto: {{film.vote_average}}</li>
+                            </ul>
+                        </div>
+                    </div>  
               </div>
           </div>
       </div>
@@ -22,6 +32,13 @@ export default {
     props : {
         films: Array,
         campoRicerca: String,
+    },
+    methods :{
+        capitalize(string) {
+           
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        },
+
     }
 }
 </script>
